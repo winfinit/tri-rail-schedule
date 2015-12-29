9a87d94b3ce6b311bfb754a64b59a9f98906b67c
Roman Jurkov <winfinit@gmail.com>
2015-12-29 17:18:32 -0500

    added readme

--- test.js
+++ test.js
@@ -2,10 +2,11 @@ var TriRailSchedule = require('./');
 
 var times = TriRailSchedule.getByDepartureIdAndArrivaldAndScheduleId(14, 6, TriRailSchedule.weekday);
 
+
 console.log("total records:", times.length);
 times.forEach(function(row, index) {
     console.log('------');
     console.log("%d:", index+1, "station name:", TriRailSchedule.getStationName(row.departure_station_id));
     console.log("%d:", index+1, "departure time:", TriRailSchedule.formatTime(row.departure_time));
-    console.log("%d:", index+1, "arrival time:", TriRailSchedule.formatTime(row.arrival_time));
+    console.log("%d:", index+1, "arrival time:", TriRailSchedule.formatTime(row.arrival_time), row.arrival_time);
 });
